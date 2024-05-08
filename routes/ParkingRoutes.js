fe5e6f0b1eb70d168be1f45e2222bb3c8be150b2
alfito69420 <panchopistolas159753@gmail.com>
2024-05-08 11:30:56 -0600

    feat: actualizacion de datos del producto

--- routes/ParkingRoutes.js
+++ routes/ParkingRoutes.js
@@ -21,6 +21,31 @@ router.get('/get-all-products', (req, res) => {
   });
 
 //  UPDATE PRODUCTOS
+router.post('/update/:id', (req, res) => {
+    var update_producto_sql = "UPDATE producto SET nombre = ?, descripcion = ?, precio = ?, cantidad = ?, photo = ?, id_cat_producto = ?";
+  
+    var values = [
+      req.body.nombre,
+      req.body.descripcion,
+      req.body.precio,
+      req.body.cantidad,
+      req.body.photo,
+      req.body.id_cat_producto,
+      // El ID del vehículo se obtiene de los parámetros de la URL
+      req.params.id
+    ];
+  
+    console.log(req.body);
+  
+    connection.query(update_producto_sql, values, function (err, result) {
+      if (err) {
+        return res.status(500).json(err);
+      } else {
+        console.log("Number of records updated: " + result.affectedRows);
+        return res.status(200).json(req.body);
+      }
+    });
+  })
 
 //  CREATE PRODUCTOS
 
