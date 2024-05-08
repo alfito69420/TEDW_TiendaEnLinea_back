df237e562210d02edc342dc3ec762894345a4636
alfito69420 <panchopistolas159753@gmail.com>
2024-05-08 11:22:35 -0600

    feat: select de todos los productos

--- routes/ParkingRoutes.js
+++ routes/ParkingRoutes.js
@@ -3,8 +3,28 @@ let router = express.Router();
 
 let connection = require('../database/MySQLConnections').databaseConnection;
 
+//  SELECT PRODUCTOS
+router.get('/get-all-products', (req, res) => {
+    connection.query('SELECT * FROM producto', (err, rows, fields) => {
+      if (err) throw err;
+  
+      rows.forEach(row => {
+        console.log('ID:', row.id_producto); // Cambia 'id' al nombre real del campo
+        console.log('Producto:', row.nombre); // Cambia 'nombre' al nombre real del campo
+      });
+      console.log(rows);
+  
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(rows);
+    });
+  
+  });
 
+//  UPDATE PRODUCTOS
 
+//  CREATE PRODUCTOS
+
+//  DELETE PRODUCTOS
 
 
 module.exports = router;
